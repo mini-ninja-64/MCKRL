c0e2c898bb08fcd4f6ca82b620c37e1dba4f5c77
mini_ninja_64 <mini-ninja-64@users.noreply.github.com>
2025-10-11 18:02:15 +0100

    Move defs out of src

--- util/build-lib.sh
+++ util/build-lib.sh
@@ -7,20 +7,24 @@ SOURCE_DIR="$(dirname "$SCRIPT_PATH")/../src"
 
 cd "$SOURCE_DIR" || return 1
 
+PROJECT_DIR="${SOURCE_DIR}/.."
+
 YAML_GENERATOR_SCRIPT="$SOURCE_DIR/generate_from_yaml.py"
-DEFINITIONS_DIR="$SOURCE_DIR/definitions"
+DEFINITIONS_DIR="$PROJECT_DIR/definitions"
+FIXED_KICAD_FOOTPRINTS_DIR="$PROJECT_DIR/constant"
+
 GENERATORS_DIR="$SOURCE_DIR/generators"
-GENERATED_DIR="$SOURCE_DIR/../generated"
-FIXED_KICAD_OBJECTS_DIR="$SOURCE_DIR/constant"
+GENERATED_DIR="$PROJECT_DIR/generated"
+
 
 # Delete generated dir
 echo "Cleaning generated directory..."
 rm -rf "$GENERATED_DIR"
 
 # Copy kicad objects from hardcoded folders
-if [ -d "$FIXED_KICAD_OBJECTS_DIR" ]; then
-  echo "Copying constant footprints..."
-	cp --recursive "$FIXED_KICAD_OBJECTS_DIR" "$GENERATED_DIR"
+if [ -d "$FIXED_KICAD_FOOTPRINTS_DIR" ]; then
+	echo "Copying constant footprints..."
+	cp --recursive "$FIXED_KICAD_FOOTPRINTS_DIR" "$GENERATED_DIR"
 fi
 
 # Generate kicad objects
